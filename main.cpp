05eb0eb6983b1531becaeb54a5c5211aba5fe9d6
nicolas-trouin <nicolas.trouin@insa-lyon.fr>
2020-05-06 17:24:21 +0200

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,231 @@
+#include <iostream>
+#include <limits>
+
+#include "Service/CSVParser.h"
+#include "Metier/Data/Catalog.h"
+#include "Metier/Acteurs/PrivateIndividual.h"
+#include "Metier/Acteurs/Provider.h"
+#include "Metier/Acteurs/Decider.h"
+#include "Metier/Acteurs/SuperUser.h"
+
+using namespace std;
+
+void SelectPrivateIndividual(Catalog & catalog);
+void SelectProvider(Catalog & catalog);
+
+void PrivateIndividualView(PrivateIndividual & privateIndividual);
+void ProviderView(Provider & provider);
+void DeciderView(Decider & decider);
+void SuperUserView(SuperUser & superUser);
+
+void ClearBuffer()
+{
+    cin.clear ();
+    cin.ignore (numeric_limits<streamsize>::max(), '\n');
+}
+
+int main(){
+
+    cout << "Chargement des données en cours, veuillez patienter." << endl << endl;
+    Catalog catalog = new Catalog();
+    //TODO Initialiser les données
+    Decider decider = new Decider();
+    SuperUser superUser = new SuperUser();
+    cout << "Les données on bien été chargées." << endl << endl;
+
+    int commande = 0;
+    cout << "Avec quel compte voulez-vous vous connecter ?" << endl << endl;
+    for(;;)
+    {
+        cout << "Menu :" << endl
+             << "-- (1) Individu privé" << endl
+             << "-- (2) Fournisseur d'AirCleaner" << endl
+             << "-- (3) Agence Gouvernementale" << endl
+             << "-- (4) Administrateur (SuperUser)" << endl
+             << "-- (5) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+            case 1:
+                SelectPrivateIndividual(catalog);
+                break;
+            case 2:
+                SelectProvider(catalog);
+                break;
+            case 3:
+                DeciderView(decider);
+                break;
+            case 4:
+                SuperUserView(superUser);
+                break;
+            case 5:
+                cout << "Fermeture ..." << endl;
+                return 0;
+            default:
+                cerr << "Commande inconnue." << endl;
+                ClearBuffer();
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void PrivateIndividualView(PrivateIndividual & privateIndividual) {
+    int commande = 0;
+    cout << "Bienvenue " << privateIndividual.getLogin() << endl;
+    cout << "Que voulez-vous faire ?" << endl << endl;
+    for(;;)
+    {
+        cout << "Menu :" << endl
+             << "-- (1) Voir mon score" << endl
+             << "-- (2) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+            case 1:
+                cout << "Votre score est de : " << privateIndividual.getScore() << endl;
+                break;
+            case 2:
+                cout << "Fermeture ..." << endl;
+                return;
+            default:
+                cerr << "Commande inconnue." << endl;
+                ClearBuffer();
+                break;
+        }
+    }
+    return;
+}
+
+void ProviderView(Provider & provider) {
+    int commande = 0;
+    cout << "Bienvenue " << provider.getLogin() << endl;
+    cout << "Que voulez-vous faire ?" << endl << endl;
+    for(;;)
+    {
+        cout << "Menu :" << endl
+             << "-- (1) Analyser l'impact de mes AirCleaners" << endl
+             << "-- (2) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+            case 1:
+                //TODO Analyser l'impact
+                break;
+            case 2:
+                cout << "Fermeture ..." << endl;
+                return;
+            default:
+                cerr << "Commande inconnue." << endl;
+                ClearBuffer();
+                break;
+        }
+    }
+}
+
+void DeciderView(Decider & decider) {
+    int commande = 0;
+    cout << "Bienvenue" << decider.getLogin() << endl;
+    cout << "Que voulez-vous faire ?" << endl << endl;
+    for(;;)
+    {
+        cout << "Menu :" << endl
+             << "-- (1) Calculer la moyenne " << endl //TODO améliorer les prompts
+             << "-- (2) Identifier les capteurs similaires " << endl
+             << "-- (3) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+            case 1:
+                //TODO Calculer la moyenne
+                break;
+            case 2:
+                //TODO Identifier les capteurs similaires
+                break;
+            case 3:
+                cout << "Fermeture ..." << endl;
+                return;
+            default:
+                cerr << "Commande inconnue." << endl;
+                ClearBuffer();
+                break;
+        }
+    }
+}
+
+void SuperUserView(SuperUser & superUser) {
+    int commande = 0;
+    cout << "Bienvenue " << superUser.getLogin() << endl;
+    cout << "Que voulez-vous faire ?" << endl << endl;
+    for(;;)
+    {
+        cout << "Menu :" << endl
+             << "-- (1) Mesurer la performance algorithmique" << endl
+             << "-- (2) Sortir" << endl;
+        cin >> commande;
+        switch(commande)
+        {
+            case 1:
+                //TODO Mesurer la performance
+                break;
+            case 2:
+                cout << "Fermeture ..." << endl;
+                return;
+            default:
+                cerr << "Commande inconnue." << endl;
+                ClearBuffer();
+                break;
+        }
+    }
+}
+
+void SelectPrivateIndividual(Catalog & catalog) {
+    int commande = 0;
+    for(;;)
+    {
+        cout << "Choisissez l'individu privé souhaité" << endl;
+        cout << "-- (0) Sortir" << endl;
+        vector<PrivateIndividual> privateIndividuals = catalog.getPrivateIndividuals();
+        for(unsigned i(0); i < privateIndividuals.size(); ++i) {
+            cout << "-- (" << i+1 << ") " << privateIndividuals[i].getLogin() << endl;
+        }
+        cin >> commande;
+
+        if(commande == 0)
+            return;
+
+        if(commande < 1 || commande > privateIndividuals.size()){
+            cout << "Commande erronée" << endl;
+            continue;
+        }
+
+        PrivateIndividualView(privateIndividuals[commande]);
+
+    }
+}
+
+void SelectProvider(Catalog &catalog) {
+    int commande = 0;
+    for(;;)
+    {
+        cout << "Choisissez le fournisseur d'AirCleaners souhaité" << endl;
+        cout << "-- (0) Sortir" << endl;
+        vector<Provider> providers = catalog.getProviders();
+        for(unsigned i(0); i < providers.size(); ++i) {
+            cout << "-- (" << i+1 << ") " << providers[i].getLogin() << endl;
+        }
+        cin >> commande;
+
+        if(commande == 0)
+            return;
+
+        if(commande < 1 || commande > providers.size()){
+            cout << "Commande erronée" << endl;
+            continue;
+        }
+
+        ProviderView(providers[commande]);
+
+    }
+}
