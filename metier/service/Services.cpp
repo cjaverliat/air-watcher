9f7f5b59f1b16b4f9cf22e66455d96e57d2a7ef3
Charles Javerliat <charles.javerliat@gmail.com>
2020-05-06 18:13:53 +0200

    feat/Add algorithms

---
+++ metier/service/Services.cpp
@@ -0,0 +1,326 @@
+#include "Services.h"
+
+#include <cmath>
+#include <cassert>
+
+#include <algorithm>
+
+const unsigned int idxLevelsO3[9] = {29, 54, 79, 104, 129, 149, 179, 209, 239};
+const unsigned int idxLevelsSO2[9] = {39, 79, 119, 159, 199, 249, 299, 399, 499};
+const unsigned int idxLevelsNO2[9] = {29, 54, 84, 109, 134, 164, 199, 274, 399};
+const unsigned int idxLevelsPM10[9] = {6, 13, 20, 27, 34, 41, 49, 64, 79};
+
+Services::Services(Catalog &catalog) : m_catalog(catalog)
+{
+}
+
+unsigned int Services::computeATMOIndex(const Measure &measure) const
+{
+    unsigned int atmoIdx = 1;
+    const unsigned int *idxLevels;
+
+    double maxConcentration = 0;
+    std::string attributeID = "";
+
+    if (measure.getValueO3() > maxConcentration)
+    {
+        maxConcentration = measure.getValueO3();
+        attributeID = "O3";
+        idxLevels = idxLevelsO3;
+    }
+    if (measure.getValueSO2() > maxConcentration)
+    {
+        maxConcentration = measure.getValueSO2();
+        attributeID = "SO2";
+        idxLevels = idxLevelsSO2;
+    }
+    if (measure.getValueNO2() > maxConcentration)
+    {
+        maxConcentration = measure.getValueNO2();
+        attributeID = "NO2";
+        idxLevels = idxLevelsNO2;
+    }
+    if (measure.getValuePM10() > maxConcentration)
+    {
+        maxConcentration = measure.getValuePM10();
+        attributeID = "PM10";
+        idxLevels = idxLevelsPM10;
+    }
+
+    while (maxConcentration >= idxLevels[atmoIdx - 1])
+    {
+        atmoIdx += 1;
+        if (atmoIdx == 10)
+        {
+            break;
+        }
+    }
+
+    assert(atmoIdx >= 1 && atmoIdx <= 10);
+    return atmoIdx;
+}
+
+double Services::haversineDistance(const Coordinates &c1, const Coordinates &c2) const
+{
+    int r = 6371e3; //Earth radius in meters
+    double alpha = (c2.getLatitude() - c1.getLatitude()) / 2;
+    double beta = (c2.getLongitude() - c1.getLongitude()) / 2;
+    double distance = 2 * r * asin(sqrt(pow(sin(alpha), 2) + cos(c1.getLatitude()) * cos(c2.getLatitude()) * pow(sin(beta), 2)));
+
+    assert(distance >= 0);
+    return distance;
+}
+
+const Measure *Services::getLastSensorMeasure(const Sensor &sensor, time_t t) const
+{
+    assert(std::is_sorted(m_catalog.getMeasures().begin(), m_catalog.getMeasures().end()));
+
+    for (const Measure &m : m_catalog.getMeasures())
+    {
+        if (sensor == m.getSensor())
+        {
+            return &m;
+        }
+    }
+
+    return nullptr;
+}
+
+std::vector<const Measure *> Services::getSensorMeasuresInPeriod(const Sensor &sensor, time_t startTime, time_t endTime) const
+{
+    assert(difftime(endTime, startTime) >= 0);
+    assert(std::is_sorted(m_catalog.getMeasures().begin(), m_catalog.getMeasures().end()));
+
+    std::vector<const Measure *> measures;
+
+    for (const Measure &m : m_catalog.getMeasures())
+    {
+        if (sensor == m.getSensor() && difftime(m.getDate(), startTime) >= 0 && difftime(endTime, m.getDate()) >= 0)
+        {
+            measures.push_back(&m);
+        }
+    }
+
+    return measures;
+}
+
+std::vector<const Measure *> Services::getSensorMeasures(const Sensor &sensor) const
+{
+    std::vector<const Measure *> measures;
+
+    for (const Measure &m : m_catalog.getMeasures())
+    {
+        if (sensor == m.getSensor())
+        {
+            measures.push_back(&m);
+        }
+    }
+
+    return measures;
+}
+
+double Services::meanAirQuality(const Coordinates &zoneCenter, double zoneRadius, time_t t) const
+{
+    unsigned int idxSum = 0;
+    double n = 0;
+
+    for (const Sensor &sensor : m_catalog.getSensors())
+    {
+        double d = haversineDistance(zoneCenter, sensor.getCoordinates());
+
+        if (d <= zoneRadius)
+        {
+            const Measure *lastMeasure = getLastSensorMeasure(sensor, t);
+
+            if (lastMeasure != nullptr)
+            {
+                idxSum += computeATMOIndex(*lastMeasure);
+                ++n;
+            }
+        }
+    }
+
+    if (n > 0)
+    {
+        return idxSum / n;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+double Services::meanAirQuality(const Coordinates &zoneCenter, double zoneRadius, time_t startTime, time_t endTime) const
+{
+    assert(difftime(endTime, startTime) >= 0);
+
+    unsigned int idxSum = 0;
+    double n = 0;
+
+    for (const Sensor &sensor : m_catalog.getSensors())
+    {
+        double d = haversineDistance(zoneCenter, sensor.getCoordinates());
+
+        if (d <= zoneRadius)
+        {
+            std::vector<const Measure *> measures = getSensorMeasuresInPeriod(sensor, startTime, endTime);
+
+            for (const Measure *m : measures)
+            {
+                idxSum += computeATMOIndex(*m);
+                ++n;
+            }
+        }
+    }
+
+    if (n > 0)
+    {
+        return idxSum / n;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+std::vector<const Sensor *> Services::getSimilarSensors(const Sensor &refSensor, double epsilonATMOIndex, double epsilonTime) const
+{
+    assert(std::is_sorted(m_catalog.getMeasures().begin(), m_catalog.getMeasures().end()));
+
+    std::vector<const Sensor *> similarSensors;
+
+    std::vector<const Measure *> refSensorMeasures = getSensorMeasures(refSensor);
+    assert(std::is_sorted(refSensorMeasures.begin(), refSensorMeasures.end()));
+
+    for (const Sensor &sensor : m_catalog.getSensors())
+    {
+        if (sensor == refSensor)
+            continue;
+
+        bool similarMeasures = true;
+        std::vector<const Measure *> currentSensorMeasures = getSensorMeasures(sensor);
+        assert(std::is_sorted(currentSensorMeasures.begin(), currentSensorMeasures.end()));
+
+        if (currentSensorMeasures.size() == 0)
+        {
+            continue;
+        }
+
+        for (auto i = 0; i < std::min(refSensorMeasures.size(), currentSensorMeasures.size()); ++i)
+        {
+            const Measure &refMeasure = *refSensorMeasures.at(i);
+            const Measure &currentSensorMeasure = *currentSensorMeasures.at(i);
+            bool atmoIndexSimilar = abs(computeATMOIndex(refMeasure) - computeATMOIndex(currentSensorMeasure)) <= epsilonATMOIndex;
+            bool timeSimilar = abs(difftime(refMeasure.getDate(), currentSensorMeasure.getDate())) <= epsilonTime;
+
+            if (!atmoIndexSimilar || !timeSimilar)
+            {
+                similarMeasures = false;
+                break;
+            }
+        }
+
+        if (similarMeasures)
+        {
+            similarSensors.push_back(&sensor);
+        }
+    }
+
+    return similarSensors;
+}
+
+std::pair<std::string, double> Services::getZoneCaracteristicAttribute(const Coordinates &zoneCenter, double zoneRadius, time_t t) const
+{
+    double sumO3 = 0;
+    double sumSO2 = 0;
+    double sumNO2 = 0;
+    double sumPM10 = 0;
+
+    unsigned int n = 0;
+
+    for (const Sensor &sensor : m_catalog.getSensors())
+    {
+        double d = haversineDistance(sensor.getCoordinates(), zoneCenter);
+
+        if (d <= zoneRadius)
+        {
+            const Measure &lastSensorMeasure = *getLastSensorMeasure(sensor, t);
+            sumO3 += lastSensorMeasure.getValueO3();
+            sumSO2 += lastSensorMeasure.getValueSO2();
+            sumNO2 += lastSensorMeasure.getValueNO2();
+            sumPM10 += lastSensorMeasure.getValuePM10();
+            ++n;
+        }
+    }
+
+    if (n > 0)
+    {
+        double meanO3 = sumO3 / n;
+        double meanSO2 = sumSO2 / n;
+        double meanNO2 = sumNO2 / n;
+        double meanPM10 = sumPM10 / n;
+
+        double maxConcentration = std::max(meanO3, std::max(meanSO2, std::max(meanNO2, meanPM10)));
+
+        if (maxConcentration == meanO3)
+        {
+            return std::make_pair("O3", meanO3);
+        }
+        else if (maxConcentration == meanSO2)
+        {
+            return std::make_pair("SO2", meanSO2);
+        }
+        else if (maxConcentration == meanNO2)
+        {
+            return std::make_pair("NO2", meanNO2);
+        }
+        else
+        {
+            return std::make_pair("PM10", meanPM10);
+        }
+    }
+    else
+    {
+        return std::make_pair("", -1);
+    }
+}
+
+std::pair<double, double> Services::computeAirCleanerImpact(const AirCleaner &airCleaner) const
+{
+    unsigned int improvementsSum = 0;
+    unsigned int nbImprovements = 0;
+    double impactRadius = 0;
+
+    //Sorted by increasing distance
+    std::map<double, const Sensor *> sensorsAround = m_catalog.getSensorsAroundAirCleaner(airCleaner);
+
+    for (auto &mapEntry : m_catalog.getSensorsAroundAirCleaner(airCleaner))
+    {
+        double distance = mapEntry.first; //Distance between the sensor and the air cleaner
+        const Sensor &sensor = *mapEntry.second;
+
+        unsigned int idxATMOBefore = computeATMOIndex(*getLastSensorMeasure(sensor, airCleaner.getCleanerStart()));
+        unsigned int idxATMOAfter = computeATMOIndex(*getLastSensorMeasure(sensor, time(0)));
+
+        if (idxATMOAfter > idxATMOBefore)
+        {
+            improvementsSum += idxATMOAfter - idxATMOBefore;
+            ++nbImprovements;
+            impactRadius = distance;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (nbImprovements > 0)
+    {
+        double meanImprovement = improvementsSum / nbImprovements;
+        return std::make_pair(impactRadius, meanImprovement);
+    }
+    else
+    {
+        return std::make_pair(0, 0);
+    }
+}
---
+++ metier/service/Services.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <ctime>
+#include <utility>
+#include <map>
+
+#include "Measure.h"
+#include "Coordinates.h"
+#include "Sensor.h"
+#include "Catalog.h"
+#include "AirCleaner.h"
+
+class Services
+{
+
+private:
+    Catalog &m_catalog;
+
+public:
+    Services(Catalog &catalog);
+
+    /**
+     * @brief Compute the ATMO index
+     * 
+     * Compute the ATMO index according to the table available on Wikipedia:
+     * https://fr.wikipedia.org/wiki/Indice_de_qualit%C3%A9_de_l'air#Indice_Atmo 
+     * 
+     * @param measure The measure containing the concentration of O3, SO2, NO2 and PM10 to compute the index.
+     * @return The ATMO index, between 1 and 10.
+     */
+    unsigned int computeATMOIndex(const Measure &measure) const;
+
+    /**
+     * @brief Compute the Haversine distance between two coordinates
+     * 
+     * Compute the Harversine distance between two coordinates according to the Wikipedia formula:
+     * https://fr.wikipedia.org/wiki/Formule_de_haversine
+     * 
+     * @param c1 The first coordinates (longitude, latitude).
+     * @param c2 The second coordinates (longitude, latitude).
+     * @return The distance between the two points, in meters.
+     */
+    double haversineDistance(const Coordinates &c1, const Coordinates &c2) const;
+
+    /**
+     * @brief Get the last sensor measure
+     * 
+     * @param sensor The sensor to return the last measure from.
+     * @param t The time under which we will take the last sensor measure.
+     * @return The last measure of the sensor, or nullptr if none.
+     */
+    const Measure *getLastSensorMeasure(const Sensor &sensor, time_t t) const;
+
+    /**
+     * @brief Get the sensor Measures on a given period
+     * 
+     * @param sensor The sensor to return the measures from.
+     * @param startTime The start time of the time period.
+     * @param endTime The end time of the time period.
+     * @return The list of all the measures included in the given time period.
+     */
+    std::vector<const Measure *> getSensorMeasuresInPeriod(const Sensor &sensor, time_t startTime, time_t endTime) const;
+
+    /**
+     * @brief Get all the sensor measures
+     * 
+     * @param sensor The sensor to return the measures from.
+     * @return The list of all the measures for the given sensor.
+     */
+    std::vector<const Measure *> getSensorMeasures(const Sensor &sensor) const;
+
+    /**
+     * @brief Compute the mean air quality in the given zone at a given time
+     * 
+     * Compute the mean air quality from all sensors inside the given zone.
+     * 
+     * @param zoneCenter The coordinates of the center of the zone.
+     * @param zoneRadius The radius of the zone.
+     * @param t The time under which we will take air quality from each sensor.
+     * @return The mean air quality (this is an ATMO index), between 1 and 10. 
+     */
+    double meanAirQuality(const Coordinates &zoneCenter, double zoneRadius, time_t t) const;
+    /**
+     * @brief Compute the mean air quality in the given zone over a timespan
+     * 
+     * Compute the mean air quality from all sensors inside the given zone over the given timespan.
+     * 
+     * @param zoneCenter The coordinates of the center of the zone.
+     * @param zoneRadius The radius of the zone.
+     * @param startTime The start time of the time period.
+     * @param endTime The end time of the time period.
+     * @return The mean air quality (this is an ATMO index), between 1 and 10. 
+     */
+    double meanAirQuality(const Coordinates &zoneCenter, double zoneRadius, time_t startTime, time_t endTime) const;
+
+    /**
+     * @brief Get all the similar sensors of a reference one
+     * 
+     * @param refSensor The reference sensor.
+     * @param epsilonATMOIndex Epsilon used to check if the difference of ATMO index of a reference measure and another sensor's measure is small enough.
+     * @param epsilonTime Epsilon used to check if the difference of time of a reference measure and another sensor's measure is small enough.
+     * @return The list of all sensors similar to the reference sensor.
+     */
+    std::vector<const Sensor *> Services::getSimilarSensors(const Sensor &refSensor, double epsilonATMOIndex, double epsilonTime) const;
+
+    /**
+     * @brief Get the zone caracteristic attribute
+     * 
+     * @param zoneCenter The coordinates of the center of the zone.
+     * @param zoneRadius The radius of the zone.
+     * @param t The time under which we will take the zone caracteristic attribute.
+     * @return A pair consisting of the attributeID and its average concentration in the zone.
+     */
+    std::pair<std::string, double> getZoneCaracteristicAttribute(const Coordinates &zoneCenter, double zoneRadius, time_t t) const;
+
+    /**
+     * @brief Compute the impact of an air cleaner
+     * 
+     * @param airCleaner The air cleaner to compute the impact of.
+     * @return A pair consisting of the impact radius and the average ATMO index improvement.
+     */
+    std::pair<double, double> computeAirCleanerImpact(const AirCleaner &airCleaner) const;
+};
